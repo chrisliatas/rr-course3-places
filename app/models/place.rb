329d6b5eddcf757572aa757691f4aa3a2ede73d8
classhopper <classhopp3r@gmail.com>
2018-02-25 21:25:20 +0200

    rspec rq02 OK

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,15 @@
 class Place
   include Mongoid::Document
+
+  def self.mongo_client
+    Mongoid::Clients.default
+  end
+
+  def self.collection
+    mongo_client[:places]
+  end
+
+  def self.load_all(file)
+    collection.insert_many(JSON.parse(file.read))
+  end
 end
