c9893f064cd7efdff8983ae9614f4d14c60ae808
classhopper <classhopp3r@gmail.com>
2018-02-26 22:24:10 +0200

    rspec photos_spec rq05 OK changed Gem exifir to 1.2.6

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,5 @@
 class Photo
+  include Mongoid::Document
   attr_accessor :id, :location
   attr_writer :contents
 
@@ -7,8 +8,32 @@ class Photo
     @location = Point.new(params[:metadata][:location]) unless params.nil?
     @place = params[:metadata][:place] unless params.nil?
   end
-  
+
   def self.mongo_client
     Mongoid::Clients.default
+  end
+
+  def persisted?
+    !@id.nil?
+  end
+
+  def save
+    if !persisted?
+      gps = EXIFR::JPEG.new(@contents).gps
+      location = Point.new(lng: gps.longitude, lat: gps.latitude)
+      description = {}
+      description[:content_type] = "image/jpeg"
+      description[:metadata] = { location: location.to_hash, place: @place }
+      @location = Point.new(location.to_hash)
+      @contents.rewind
+      grid_file = Mongo::Grid::File.new(@contents.read, description)
+      id = Place.mongo_client.database.fs.insert_one(grid_file)
+      @id = id.to_s
+    else
+      doc = Photo.mongo_client.database.fs.find(_id: BSON::ObjectId.from_string(@id)).first
+      doc[:metadata][:location] = @location.to_hash
+      doc[:metadata][:place] = @place
+      Photo.mongo_client.database.fs.find(_id: BSON::ObjectId.from_string(@id)).update_one(doc)
+    end
   end
 end
