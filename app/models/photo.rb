228a051ac66a18d9a65b06dec5576abd0006d6d7
classhopper <classhopp3r@gmail.com>
2018-02-26 21:23:18 +0200

    rspec photos_spec rq03 OK

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,12 @@
 class Photo
   attr_accessor :id, :location
   attr_writer :contents
+
+  def initialize(params = nil)
+    @id = params[:_id].to_s unless params.nil?
+    @location = Point.new(params[:metadata][:location]) unless params.nil?
+    @place = params[:metadata][:place] unless params.nil?
+  end
   
   def self.mongo_client
     Mongoid::Clients.default
