3516501286013ef4d4d775e7944770828a7ccc34
classhopper <classhopp3r@gmail.com>
2018-02-26 19:02:40 +0200

    rspec query_spec rq01 OK

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,6 +3,13 @@ class Place
 
   attr_accessor :id, :formatted_address, :location, :address_components
 
+  def initialize(params)
+    @id = params[:_id].to_s
+    @formatted_address = params[:formatted_address]
+    @location = Point.new(params[:geometry][:geolocation])
+    @address_components = params[:address_components].map { |a| AddressComponent.new(a) } unless params[:address_components].nil?
+  end
+
   def self.mongo_client
     Mongoid::Clients.default
   end
@@ -15,11 +22,7 @@ class Place
     collection.insert_many(JSON.parse(file.read))
   end
 
-  def initialize(params)
-    @id = params[:_id].to_s
-    @formatted_address = params[:formatted_address]
-    @location = Point.new(params[:geometry][:geolocation])
-    @address_components = params[:address_components]
-                              .map{ |a| AddressComponent.new(a)} if !params[:address_components].nil?
+  def self.find_by_short_name(short_name)
+    collection.find({"address_components.short_name" => short_name})
   end
 end
