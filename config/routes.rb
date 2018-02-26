0fa847f74f360b70b70fbd410bce3282f9db5422
classhopper <classhopp3r@gmail.com>
2018-02-26 23:01:01 +0200

    rspec images_spec OK

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :places
+  root 'places#index'
+  resources :places, only: [:index, :show]
+  get 'photos/:id/show', to: 'photos#show', as: 'photos_show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
